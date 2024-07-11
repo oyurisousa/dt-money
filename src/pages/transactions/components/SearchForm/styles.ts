314dc3399b77b686067217203e4a333378eaff9c
yuri <yurisousaenfer@gmail.com>
2024-07-11 16:36:08 -0300

    chore: table transactions & search form

---
+++ src/pages/transactions/components/SearchForm/styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+export const SearchFormContainer = styled.form`
+  display: flex;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+    border-radius: 6px;
+    border: 0;
+    background-color: ${(props) => props.theme['gray-600']};
+    color: ${(props) => props.theme['gray-300']};
+    padding: 1rem;
+
+    &::placeholder {
+      color: ${(props) => props.theme['gray-5 00']};
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.75rem;
+    cursor: pointer;
+
+    border: 0;
+    padding: 1rem;
+    background-color: transparent;
+    border: 1px solid ${(props) => props.theme['green-300']};
+    color: ${(props) => props.theme['green-300']};
+    font-weight: bold;
+    border-radius: 6px;
+
+    &:hover {
+      background-color: ${(props) => props.theme['green-500']};
+      border-color: ${(props) => props.theme['green-500']};
+      color: ${(props) => props.theme.white};
+      transition:
+        background-color 0.2s,
+        color 0.2s,
+        border-color 0.2s;
+    }
+  }
+`
--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,11 +1,48 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 14.000</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>11/07/2024</td>
+            </tr>
+            <tr>
+              <td width="40%">Cachorro Quente</td>
+              <td>
+                <PriceHighLight variant="outcome">R$ 4.000</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>11/07/2024</td>
+            </tr>
+            <tr>
+              <td width="40%">bolsa</td>
+              <td>
+                <PriceHighLight variant="income">R$ 1000</PriceHighLight>
+              </td>
+              <td>Auxílio</td>
+              <td>11/07/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
