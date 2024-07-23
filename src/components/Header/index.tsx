d1ddcc29d6b4cb5e37a3681953b536271591d5d0
yuri <yurisousaenfer@gmail.com>
2024-07-23 20:28:26 -0300

    chore: modal new transaction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,19 @@
 import { HeaderContainer, HeaderContent, NewTranssactionButton } from './styles'
 import logo from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal'
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logo} alt="" />
-        <NewTranssactionButton>New Transaction</NewTranssactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTranssactionButton>New Transaction</NewTranssactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
